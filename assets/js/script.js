ded06f54943d376ee792229e50df9e36132ef67f
tarik besic <tarikdalmatia@gmail.comm>
2023-05-19 15:43:02 +0200

    started reviews section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const scroller = document.querySelector(".hero__scroller");
 const firstSection = document.querySelector("#section--first");
 const secondSection = document.querySelector("#section--second");
+const thirdSection = document.querySelector("#section--third");
+const scrollContainer = document.querySelector(".reviews__posts-wrapper");
 
 const scrollToSection = () => {
     firstSection.scrollIntoView({
@@ -16,11 +18,17 @@ const handleMargin = () => {
 
     const properMargin = (clientWidth - width) / 2;
     secondSection.style.marginLeft = `${properMargin}px`;
+    thirdSection.style.marginRight = `${properMargin}px`;
     console.log(width, clientWidth);
     console.log(secondSection.style.marginLeft);
 };
 
-scroller.addEventListener("click", scrollToSection);
+const handlePostsScroll = (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+};
 
+scroller.addEventListener("click", scrollToSection);
+scrollContainer.addEventListener("wheel", (e) => handlePostsScroll(e));
 window.addEventListener("load", handleMargin);
 window.addEventListener("resize", handleMargin);
