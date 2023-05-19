cc07222c550ad148aa7c655815cececd7259ca7b
tarik besic <tarikdalmatia@gmail.comm>
2023-05-19 13:29:09 +0200

    fixed second sections margin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const scroller = document.querySelector(".scroller");
+const scroller = document.querySelector(".hero__scroller");
 const firstSection = document.querySelector("#section--first");
+const secondSection = document.querySelector("#section--second");
 
 const scrollToSection = () => {
     firstSection.scrollIntoView({
@@ -7,4 +8,19 @@ const scrollToSection = () => {
     });
 };
 
+const handleMargin = () => {
+    const width = firstSection.offsetWidth;
+    const clientWidth = document.documentElement.clientWidth;
+
+    if (clientWidth > 1920) return;
+
+    const properMargin = (clientWidth - width) / 2;
+    secondSection.style.marginLeft = `${properMargin}px`;
+    console.log(width, clientWidth);
+    console.log(secondSection.style.marginLeft);
+};
+
 scroller.addEventListener("click", scrollToSection);
+
+window.addEventListener("load", handleMargin);
+window.addEventListener("resize", handleMargin);
