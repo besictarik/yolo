953b2e5bfd4e7efa686fa9ed90ba9b613e4df70b
tarik besic <tarikdalmatia@gmail.comm>
2023-05-23 15:39:52 +0200

    working on slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+//Scroll Animation
 const scroller = document.querySelector(".hero__scroller");
 const firstSection = document.querySelector("#section--first");
+
+//Margin handling
 const secondSection = document.querySelector("#section--second");
 const thirdSection = document.querySelector("#section--third");
 const scrollContainer = document.querySelector(".reviews__posts-wrapper");
+
+//Slider
+const leftArrow = document.querySelector("#slider-arrow--left");
+const rightArrow = document.querySelector("#slider-arrow--right");
 
 const scrollToSection = () => {
     firstSection.scrollIntoView({
@@ -19,8 +26,6 @@ const handleMargin = () => {
     const properMargin = (clientWidth - width) / 2;
     secondSection.style.marginLeft = `${properMargin}px`;
     thirdSection.style.marginRight = `${properMargin}px`;
-    console.log(width, clientWidth);
-    console.log(secondSection.style.marginLeft);
 };
 
 const handlePostsScroll = (evt) => {
@@ -28,7 +33,24 @@ const handlePostsScroll = (evt) => {
     scrollContainer.scrollLeft += evt.deltaY;
 };
 
+let isFullyRight = false;
+
+const scrollSlider = (direction) => {
+    const snap = scrollContainer.children[0].offsetWidth;
+    const containerWidth = scrollContainer.offsetWidth;
+    const extraSnap = containerWidth % snap;
+
+    console.log(containerWidth);
+    if (direction == "right") {
+        scrollContainer.scrollBy({ left: snap, top: 0, behavior: "smooth" });
+    } else {
+        scrollContainer.scrollBy({ left: -extraSnap, top: 0, behavior: "smooth" });
+    }
+};
+
 scroller.addEventListener("click", scrollToSection);
-scrollContainer.addEventListener("wheel", (e) => handlePostsScroll(e));
+// scrollContainer.addEventListener("wheel", (e) => handlePostsScroll(e));
 window.addEventListener("load", handleMargin);
 window.addEventListener("resize", handleMargin);
+leftArrow.addEventListener("click", () => scrollSlider("left"));
+rightArrow.addEventListener("click", () => scrollSlider("right"));
